cea2a906304db28457089f02058b5b2df197c79b
mot9lXVII <romahohol1337w@gmail.com>
2025-03-23 23:32:27 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,18 +1,23 @@
+// scripts.js
 
-document.addEventListener('DOMContentLoaded', function () {
+// Получаем элементы
+const menuToggle = document.getElementById('menu-toggle');
+const closeMenu = document.getElementById('close-menu');
+const sidebar = document.getElementById('sidebar');
 
-    document.querySelectorAll('a').forEach(link => {
-        link.addEventListener('click', function (e) {
+// Открываем меню
+menuToggle.addEventListener('click', () => {
+    sidebar.classList.add('active');
+});
 
-            e.preventDefault();
+// Закрываем меню
+closeMenu.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+});
 
-            document.body.style.opacity = '0';
-            document.body.style.transition = 'opacity 0.3s ease';
-
-
-            setTimeout(() => {
-                window.location.href = link.href;
-            }, 300); 
-        });
-    });
+// Закрываем меню при клике вне его
+document.addEventListener('click', (event) => {
+    if (!sidebar.contains(event.target) && !menuToggle.contains(event.target)) {
+        sidebar.classList.remove('active');
+    }
 });
