1a6cb4289bffe63aed747a0c4d37ff3dd8a688dc
mot9lXVII <romahohol1337w@gmail.com>
2025-03-24 02:00:03 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -24,10 +24,23 @@ document.addEventListener('click', (event) => {
 const themeSwitcher = document.querySelector('.theme-switch');
 const body = document.body;
 
+// Проверяем сохраненную тему в localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-theme');
+    document.getElementById('dark-theme').checked = true;
+} else {
+    body.classList.remove('dark-theme');
+    document.getElementById('light-theme').checked = true;
+}
+
+// Обработка изменения темы
 themeSwitcher.addEventListener('change', (event) => {
     if (event.target.value === 'dark') {
         body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark'); // Сохраняем тему в localStorage
     } else {
         body.classList.remove('dark-theme');
+        localStorage.setItem('theme', 'light'); // Сохраняем тему в localStorage
     }
 });
