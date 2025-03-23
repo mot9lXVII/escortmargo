544ff5f9299db35f716c037389065661e0214186
mot9lXVII <romahohol1337w@gmail.com>
2025-03-23 23:27:38 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -1,19 +1,18 @@
-// Плавный переход между страницами
+
 document.addEventListener('DOMContentLoaded', function () {
-    // Добавляем обработчик для всех ссылок
+
     document.querySelectorAll('a').forEach(link => {
         link.addEventListener('click', function (e) {
-            // Отменяем стандартное поведение ссылки
+
             e.preventDefault();
 
-            // Плавное исчезновение страницы
             document.body.style.opacity = '0';
             document.body.style.transition = 'opacity 0.3s ease';
 
-            // Переход на новую страницу после завершения анимации
+
             setTimeout(() => {
                 window.location.href = link.href;
-            }, 300); // 300ms — время анимации
+            }, 300); 
         });
     });
 });
